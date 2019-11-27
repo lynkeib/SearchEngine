09787cde636e5bb4f10ff99e957516d9ff259e80
lynkeib <liuchengyin1234@126.com>
2019-11-27 14:08:20 -0800

    Login Simulating

--- ArticleSpider/ArticleSpider/spiders/zhihu.py
+++ ArticleSpider/ArticleSpider/spiders/zhihu.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from selenium import webdriver
+import time
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
 
 
 class ZhihuSpider(scrapy.Spider):
@@ -12,11 +15,34 @@ class ZhihuSpider(scrapy.Spider):
         pass
 
     def start_requests(self):
-        browser = webdriver.Chrome(executable_path="/Users/chengyinliu/D/DevelopmentProject/chromedriver")
+        # Initiate Chrome manually
+        '''
+        terminal command: --remote-debugging-port=9222
+        '''
+
+        chrome_option = Options()
+        chrome_option.add_argument("--disable-extensions")
+        chrome_option.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+
+        browser = webdriver.Chrome(executable_path="/Users/chengyinliu/D/DevelopmentProject/chromedriver",
+                                   chrome_options=chrome_option)
+
+        # browser = webdriver.Chrome(executable_path="/Users/chengyinliu/D/DevelopmentProject/chromedriver")
         browser.get('https://www.zhihu.com/signin')
+        browser.find_element_by_xpath("//div[@class='SignFlow-tab']").click()
+        # browser.find_element_by_xpath("//div[@class='SignFlow-account']//input[@class='Input']").clear()
+
+        browser.find_element_by_xpath("//div[@class='SignFlow-account']//input[@class='Input']").send_keys(
+            Keys.CONTROL + 'a')
+
         browser.find_element_by_xpath("//div[@class='SignFlow-account']//input[@class='Input']").send_keys(
             "15810952153")
+
+        browser.find_element_by_xpath("//div[@class='SignFlow-password']//input[@class='Input']").send_keys(
+            Keys.CONTROL + 'a')
+
         browser.find_element_by_xpath("//div[@class='SignFlow-password']//input[@class='Input']").send_keys("123456")
 
         browser.find_element_by_xpath(
             "//button[@class='Button SignFlow-submitButton Button--primary Button--blue']").click()
+        time.sleep(60)
