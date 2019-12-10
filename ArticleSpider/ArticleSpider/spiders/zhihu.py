12d3d1f63f0644cac1238d6010136fdeb85231b1
lynkeib <liuchengyin1234@126.com>
2019-12-10 14:08:21 -0800

    Update

--- ArticleSpider/ArticleSpider/spiders/zhihu.py
+++ ArticleSpider/ArticleSpider/spiders/zhihu.py
@@ -37,37 +37,37 @@ class ZhihuSpider(scrapy.Spider):
         browser = webdriver.Chrome(executable_path="/Users/chengyinliu/D/DevelopmentProject/chromedriver",
                                    chrome_options=chrome_option)
 
-        # # browser = webdriver.Chrome(executable_path="/Users/chengyinliu/D/DevelopmentProject/chromedriver")
-        # browser.get('https://www.zhihu.com/signin')
-        # browser.find_element_by_xpath("//div[@class='SignFlow-tab']").click()
-        # # browser.find_element_by_xpath("//div[@class='SignFlow-account']//input[@class='Input']").clear()
-        #
-        # browser.find_element_by_xpath("//div[@class='SignFlow-account']//input[@class='Input']").send_keys(
-        #     Keys.CONTROL + 'a')
-        #
-        # browser.find_element_by_xpath("//div[@class='SignFlow-account']//input[@class='Input']").send_keys(
-        #     config('ZHIHU_USERNAME'))
-        #
-        # browser.find_element_by_xpath("//div[@class='SignFlow-password']//input[@class='Input']").send_keys(
-        #     Keys.CONTROL + 'a')
-        #
-        # browser.find_element_by_xpath("//div[@class='SignFlow-password']//input[@class='Input']").send_keys(
-        #     config('ZHIHU_PASSWORD'))
-        #
-        # # browser.find_element_by_xpath(
-        # #     "//button[@class='Button SignFlow-submitButton Button--primary Button--blue']").click()
-        #
-        # loc = browser.find_element_by_xpath(
-        #     "//button[@class='Button SignFlow-submitButton Button--primary Button--blue']").location
-        #
-        # m.click(loc['x'], loc['y'])
+        # browser = webdriver.Chrome(executable_path="/Users/chengyinliu/D/DevelopmentProject/chromedriver")
+        browser.get('https://www.zhihu.com/signin')
+        browser.find_element_by_xpath("//div[@class='SignFlow-tab']").click()
+        # browser.find_element_by_xpath("//div[@class='SignFlow-account']//input[@class='Input']").clear()
 
-        browser.get("https://www.zhihu.com")
-        cookies = browser.get_cookies()
-        pickle.dump(cookies, open("../../cookies/zhihu.cookie", 'wb'))
-        cookie_dict = dict()
-        for cookie in cookies:
-            cookie_dict[cookie['name']] = cookie['value']
+        browser.find_element_by_xpath("//div[@class='SignFlow-account']//input[@class='Input']").send_keys(
+            Keys.CONTROL + 'a')
+
+        browser.find_element_by_xpath("//div[@class='SignFlow-account']//input[@class='Input']").send_keys(
+            config('ZHIHU_USERNAME'))
+
+        browser.find_element_by_xpath("//div[@class='SignFlow-password']//input[@class='Input']").send_keys(
+            Keys.CONTROL + 'a')
+
+        browser.find_element_by_xpath("//div[@class='SignFlow-password']//input[@class='Input']").send_keys(
+            config('ZHIHU_PASSWORD'))
+
+        # browser.find_element_by_xpath(
+        #     "//button[@class='Button SignFlow-submitButton Button--primary Button--blue']").click()
+
+        loc = browser.find_element_by_xpath(
+            "//button[@class='Button SignFlow-submitButton Button--primary Button--blue']").location
+
+        m.click(loc['x'], loc['y'])
+
+        # browser.get("https://www.zhihu.com")
+        # cookies = browser.get_cookies()
+        # pickle.dump(cookies, open("../../cookies/zhihu.cookie", 'wb'))
+        # cookie_dict = dict()
+        # for cookie in cookies:
+        #     cookie_dict[cookie['name']] = cookie['value']
 
         return [scrapy.Request(url=self.start_urls[0], dont_filter=True, cookies=cookie_dict)]
 
